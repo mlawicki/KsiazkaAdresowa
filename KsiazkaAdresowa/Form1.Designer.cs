5d00ce65586f302542a4ab2d032f0e9e26a9bb6d
Michał Ławicki <michal.lawicki.1988@gmail.com>
2018-02-26 14:58:14 +0100

    Add files via upload
    -Aplikacja "Ksiażka adresowa"

---
+++ KsiazkaAdresowa/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace KsiazkaAdresowa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dodatkoweBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.adresBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imieBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.usuńToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 318);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Osoba";
+            // 
+            // listView1
+            // 
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(12, 17);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(200, 295);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dodatkoweBox);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.emailBox);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.adresBox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.imieBox);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(236, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(267, 282);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Informacje";
+            // 
+            // dodatkoweBox
+            // 
+            this.dodatkoweBox.Location = new System.Drawing.Point(21, 208);
+            this.dodatkoweBox.Multiline = true;
+            this.dodatkoweBox.Name = "dodatkoweBox";
+            this.dodatkoweBox.Size = new System.Drawing.Size(231, 64);
+            this.dodatkoweBox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Dodatkowe informacje:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Urodziny:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(91, 152);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(160, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(88, 106);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(164, 20);
+            this.emailBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "E-mail:";
+            // 
+            // adresBox
+            // 
+            this.adresBox.Location = new System.Drawing.Point(88, 61);
+            this.adresBox.Name = "adresBox";
+            this.adresBox.Size = new System.Drawing.Size(164, 20);
+            this.adresBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Adres:";
+            // 
+            // imieBox
+            // 
+            this.imieBox.Location = new System.Drawing.Point(88, 18);
+            this.imieBox.Name = "imieBox";
+            this.imieBox.Size = new System.Drawing.Size(164, 20);
+            this.imieBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Imię i nazwisko:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Utwórz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(330, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Zmień";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(415, 292);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Usuń";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuńToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(99, 26);
+            // 
+            // usuńToolStripMenuItem
+            // 
+            this.usuńToolStripMenuItem.Name = "usuńToolStripMenuItem";
+            this.usuńToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.usuńToolStripMenuItem.Text = "Usuń";
+            this.usuńToolStripMenuItem.Click += new System.EventHandler(this.usuńToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 325);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Książka Adresowa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox adresBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox imieBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox emailBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox dodatkoweBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem usuńToolStripMenuItem;
+    }
+}
+
---
+++ KsiazkaAdresowa/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Text.RegularExpressions;
+
+
+namespace KsiazkaAdresowa
+{
+    public partial class Form1 : Form
+    {
+        public string ConnectionString = "Server=Localhost;Database=KsiazkaAdresowa;Uid=root;Pwd=";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            przeladujBazeDanych();
+        }
+
+        public void SQL(string command)
+        {
+
+
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(ConnectionString);
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(command, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+                imieBox.Clear();
+                adresBox.Clear();
+                emailBox.Clear();
+                dateTimePicker1.Value = DateTime.Now;
+
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message.ToString()); }
+        }
+
+        public void przeladujBazeDanych()
+        {
+            listView1.Clear();
+
+            MySqlConnection connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+            string query = "SELECT * FROM ludzie";
+            MySqlDataAdapter da = new MySqlDataAdapter(query, connection);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            connection.Close();
+
+            foreach (DataRow row in ds.Tables[0].Rows)
+            {
+                ListViewItem item = new ListViewItem(row[0].ToString());
+                item.SubItems.Add(row[1].ToString());
+                item.SubItems.Add(row[2].ToString());
+                item.SubItems.Add(row[3].ToString());
+                item.SubItems.Add(row[4].ToString());
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string imie, adres, email, data, dodatkowe;
+                imie = imieBox.Text;
+                adres = adresBox.Text;
+                email = emailBox.Text;
+                data = dateTimePicker1.Value.ToString();
+                dodatkowe = dodatkoweBox.Text;
+
+                string cmd = "INSERT INTO ludzie (imie, adres, mail, urodziny, dodatkowe) VALUES ('" + imie + "', '" + adres + "', '" + email + "', '" + data + "', '" + dodatkowe + "')";
+                SQL(cmd);
+                przeladujBazeDanych();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+
+            }
+        }
+
+        private void usuńToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.SelectedItems[0];
+            string cmd = "DELETE FROM ludzie WHERE imie='" + item.SubItems[0].Text.ToString() + "' AND adres='"+item.SubItems[1].Text.ToString() + "' AND mail='"+item.SubItems[2].Text.ToString()+"'";
+            SQL(cmd);
+            listView1.Items.Remove(item);
+            przeladujBazeDanych();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           // try
+           // {
+                ListViewItem item = listView1.SelectedItems[0];
+                imieBox.Text = item.SubItems[0].Text.ToString();
+                adresBox.Text = item.SubItems[1].Text.ToString();
+                emailBox.Text = item.SubItems[2].Text.ToString();
+                string data = item.SubItems[3].Text.ToString();
+                dateTimePicker1.Value = DateTime.Parse(data);
+                dodatkoweBox.Text = item.SubItems[4].Text.ToString();
+           // }
+           // catch (Exception ex)
+           // { MessageBox.Show(ex.Message.ToString()); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.SelectedItems[0];
+            string cmd = "DELETE FROM ludzie WHERE imie='" + item.SubItems[0].Text.ToString() + "' AND adres='" + item.SubItems[1].Text.ToString() + "' AND mail='" + item.SubItems[2].Text.ToString() + "'";
+            SQL(cmd);
+            listView1.Items.Remove(item);
+            przeladujBazeDanych();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ListViewItem item = listView1.SelectedItems[0];
+            item.SubItems[0].Text = imieBox.Text;
+            item.SubItems[1].Text = adresBox.Text;
+            item.SubItems[2].Text = emailBox.Text;
+            item.SubItems[3].Text = dateTimePicker1.Value.ToString();
+            item.SubItems[4].Text = dodatkoweBox.Text;
+
+            string cmd = "UPDATE `ludzie` SET `adres`='"+adresBox.Text+ "',`mail`='" + emailBox.Text + ",`dodatkowe`='" + dodatkoweBox.Text + " WHERE 'imie' = '" + imieBox.Text+"' AND 'urodziny' = '"+dateTimePicker1.Value.ToString()+"'";
+            SQL(cmd);
+            przeladujBazeDanych();
+        }
+
+      /*public static bool poprawnoscMaila(string email)
+        {
+            string expresion;
+            expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+            if (Regex.IsMatch(email, expresion))
+            {
+                if (Regex.Replace(email, expresion, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }*/
+    }
+}
